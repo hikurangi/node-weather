13c7278af1092bf1559873515e19b27cb56a5d67
hikurangi <hikurangi@gmail.com>
2017-07-21 16:05:44 +1200

    url

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const request = require('request')
 const dotenv = require('dotenv').config()
 
 const apiKey = process.env.OWM_KEY
+let city = 'wellington'
+let url = `http://api.openweathermap.org/data/2.5/weather?g=${city}&appid=${apiKey}`
 
 request(url, (err, response body) => {
   if (err) {
